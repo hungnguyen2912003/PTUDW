107f1261d4aeb5fd4762ee8b728b6fb8701f2a67
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2023-09-28 12:23:54 +0700

    Init

--- THPTUDWeb/MyClass/Properties/Settings.Designer.cs
+++ THPTUDWeb/MyClass/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace MyClass.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-UFMTFHO\\MSSQLSERVER19;Initial Catalog=master;Integrated Secur" +
-            "ity=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-SCOBIC5\\MSSQLSERVER2019;Initial Catalog=master;Integrated Sec" +
+            "urity=True")]
         public string StrConnect {
             get {
                 return ((string)(this["StrConnect"]));
