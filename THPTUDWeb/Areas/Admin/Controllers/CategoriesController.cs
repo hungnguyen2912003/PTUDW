bb12fd27260c040b2a7ec4ae423a44ea36538258
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2023-10-05 23:59:35 +0700

    Init

--- THPTUDWeb/Areas/Admin/Controllers/CategoriesController.cs
+++ THPTUDWeb/Areas/Admin/Controllers/CategoriesController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using MyClass.Model;
 using MyClass.DAO;
+using THPTUDWeb.Library;
 
 namespace THPTUDWeb.Areas.Admin.Controllers
 {
@@ -44,6 +45,8 @@ namespace THPTUDWeb.Areas.Admin.Controllers
 		// GET: Admin/Categories/Create
 		public ActionResult Create()
 		{
+			ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+			ViewBag.OrderList = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
 			return View();
 		}
 
@@ -56,24 +59,45 @@ namespace THPTUDWeb.Areas.Admin.Controllers
 		{
 			if (ModelState.IsValid)
 			{
+				//Xu ly cho muc Slug
+				categories.Slug = Xstring.Str_Slug(categories.Name);
+				//Xu ly cho muc ParentID
+				if (categories.ParentID == null)
+				{
+					categories.ParentID = 0;
+				}
+				//Xu ly cho muc Order
+				if (categories.Order == null)
+				{
+					categories.Order = 1;
+				}
+				else
+				{
+					categories.Order = categories.Order + 1;
+				}
+				//Xu ly cho muc CreateAt
 				categories.CreateAt = DateTime.Now;
-				categories.UpdateAt = DateTime.Now;
 				//Xu ly cho muc CreateBy
 				categories.CreateBy = Convert.ToInt32(Session["UserId"]);
+				//Xy ly cho muc UpdateAt
+				categories.UpdateAt = DateTime.Now;
 				//Xy ly cho muc UpdateBy
 				categories.UpdateBy = Convert.ToInt32(Session["UserId"]);
 				categoriesDAO.Insert(categories);
 				return RedirectToAction("Index");
 			}
-
+			ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+			ViewBag.OrderList = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
 			return View(categories);
 		}
 
 		//---------------------------------------------------------------------------------------------
 
-		// GET: Admin/Categories/Edit/5
+		// GET: Admin/Categories/Edit/5: Cap nhat mau tin
 		public ActionResult Edit(int? id)
 		{
+			ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+			ViewBag.OrderList = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
 			if (id == null)
 			{
 				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
@@ -95,9 +119,47 @@ namespace THPTUDWeb.Areas.Admin.Controllers
 		{
 			if (ModelState.IsValid)
 			{
+				//Xu ly cho muc Slug
+				categories.Slug = Xstring.Str_Slug(categories.Name);
+				//chuyen doi dua vao truong Name de loai bo dau, khoang cach = dau -
+
+				//Xu ly cho muc ParentId
+				if (categories.ParentID == null)
+				{
+					categories.ParentID = 0;
+				}
+
+				//Xu ly cho muc Order
+				if (categories.Order == null)
+				{
+					categories.Order = 1;
+				}
+				else
+				{
+					categories.Order = categories.Order + 1;
+				}
+
+				//Xu ly cho muc CreateAt
+				categories.CreateAt = DateTime.Now;
+
+				//Xu ly cho muc CreateBy
+				categories.CreateBy = Convert.ToInt32(Session["UserId"]);
+
+				//Xy ly cho muc UpdateAt
+				categories.UpdateAt = DateTime.Now;
+
+				//Xy ly cho muc UpdateBy
+				categories.UpdateBy = Convert.ToInt32(Session["UserId"]);
+
+				//Thong bao thanh cong
+				TempData["message"] = new XMessage("success", "Thêm danh mục thành công");
+
+				//Cap nhat du lieu 
 				categoriesDAO.Update(categories);
 				return RedirectToAction("Index");
 			}
+			ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+			ViewBag.OrderList = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
 			return View(categories);
 		}
 
@@ -127,5 +189,64 @@ namespace THPTUDWeb.Areas.Admin.Controllers
 			categoriesDAO.Delete(categories);
 			return RedirectToAction("Index");
 		}
+		/////////////////////////////////////////////////////////////////////////////////////
+		// GET: Admin/Category/Staus/5:Thay doi trang thai cua mau tin
+		public ActionResult Status(int? id)
+		{
+			if (id == null)
+			{
+				//Thong bao that bai
+				TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+				//chuyen huong trang
+				return RedirectToAction("Index", "Categories");
+			}
+			//khi nhap nut thay doi Status cho mot mau tin
+			Categories categories = categoriesDAO.getRow(id);
+			//Kiem tra id cua categories co ton tai?
+			if(categories == null)
+			{
+				//Thong bao that bai
+				TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+				//Thong bao thanh cong
+				TempData["message"] = new XMessage("success", "Thêm danh mục thành công");
+				//Chuyen huong trang
+				return RedirectToAction("Index", "Categories");
+			}
+			//thay doi trang thai Status tu 1 thanh 2 va nguoc lai
+			categories.Status = (categories.Status == 1) ? 2 : 1;
+			//cap nhat gia tri cho UpdateAt/By
+			categories.UpdateBy = Convert.ToInt32(Session["UserId"].ToString());
+			categories.UpdateAt = DateTime.Now;
+			//Goi ham Update trong CategoryDAO
+			categoriesDAO.Update(categories);
+			//Thong bao thanh cong
+			TempData["message"] = new XMessage("success", "Cập nhật trạng thái thành công");
+			//khi cap nhat xong thi chuyen ve Index
+			return RedirectToAction("Index", "Categories");
+		}
+		/////////////////////////////////////////////////////////////////////////////////////
+		// GET: Admin/Category/DelTrash/5:Thay doi trang thai cua mau tin = 0
+		public ActionResult DelTrash(int? id)
+		{
+			//khi nhap nut thay doi Status cho mot mau tin
+			Categories categories = categoriesDAO.getRow(id);
+
+			//thay doi trang thai Status tu 1,2 thanh 0
+			categories.Status = 0;
+
+			//cap nhat gia tri cho UpdateAt/By
+			categories.UpdateBy = Convert.ToInt32(Session["UserId"].ToString());
+			categories.UpdateAt = DateTime.Now;
+
+			//Goi ham Update trong CategoryDAO
+			categoriesDAO.Update(categories);
+
+			//Thong bao thanh cong
+			TempData["message"] = new XMessage("success", "Xóa mẩu tin thành công");
+
+			//khi cap nhat xong thi chuyen ve Index
+			return RedirectToAction("Index", "Category");
+		}
+
 	}
 }
--- THPTUDWeb/MyClass/Model/Categories.cs
+++ THPTUDWeb/MyClass/Model/Categories.cs
@@ -28,16 +28,12 @@ namespace MyClass.Model
 		[Required(ErrorMessage = "Phần từ khóa không được để trống")]
 		[Display(Name = "Từ khoá")]
 		public string MetaKey { get; set; }
-		[Required(ErrorMessage = "Phần người tạo không được để trống")]
 		[Display(Name = "Người tạo")]
 		public int CreateBy { get; set; }
-		[Required(ErrorMessage = "Phần ngày tạo không được để trống")]
 		[Display(Name = "Ngày tạo")]
 		public DateTime CreateAt { get; set; }
-		[Required(ErrorMessage = "Phần người cập nhật không được để trống")]
 		[Display(Name = "Người cập nhật")]
 		public int UpdateBy { get; set; }
-		[Required(ErrorMessage = "Phần ngày cập nhật không được để trống")]
 		[Display(Name = "Ngày cập nhật")]
 		public DateTime UpdateAt { get; set; }
 		[Required(ErrorMessage = "Phần trạng thái không được để trống")]
