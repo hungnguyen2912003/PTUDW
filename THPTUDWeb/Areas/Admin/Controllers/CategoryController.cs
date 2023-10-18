49e15a5ac7aa3603da9b198ea30b1a5ab6edbe86
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2023-10-18 20:32:41 +0700

    Init

--- THPTUDWeb/Areas/Admin/Controllers/CategoryController.cs
+++ THPTUDWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -78,6 +78,8 @@ namespace THPTUDWeb.Areas.Admin.Controllers
                 //-----UpdateBy
                 categories.UpdateBy = Convert.ToInt32(Session["UserID"]);
                 categoriesDAO.Insert(categories);
+                //Hiển thị thông báo thành công
+                TempData["message"] = new XMessage("success", "Thêm loại sản phẩm mới thành công");
                 return RedirectToAction("Index");
             }
             ViewBag.CatList = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
@@ -89,14 +91,18 @@ namespace THPTUDWeb.Areas.Admin.Controllers
         // GET: Admin/Category/Edit/5
         public ActionResult Edit(int? id)
         {
+            ViewBag.CatList = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+            ViewBag.OrderList = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+                return RedirectToAction("Index");
             }
             Categories categories = categoriesDAO.getRow(id);
             if (categories == null)
             {
-                return HttpNotFound();
+                TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+                return RedirectToAction("Index");
             }
             return View(categories);
         }
@@ -106,7 +112,35 @@ namespace THPTUDWeb.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                categoriesDAO.Update(categories);
+                //Xử lý tự động cho các trường sau:
+                //-----Create At
+                categories.CreateAt = DateTime.Now;
+                //-----Create By
+                categories.CreateBy = Convert.ToInt32(Session["UserID"]);
+                //-----Slug
+                categories.Slug = XString.Str_Slug(categories.Name);
+                //-----ParentID
+                if (categories.ParentID == null)
+                {
+                    categories.ParentID = 0;
+                }
+                //-----Order
+                if (categories.Order == null)
+                {
+                    categories.Order = 1;
+                }
+                else
+                {
+                    categories.Order += 1;
+                }
+                //-----UpdateAt
+                categories.UpdateAt = DateTime.Now;
+                //-----UpdateBy
+                categories.UpdateBy = Convert.ToInt32(Session["UserID"]);
+                categoriesDAO.Insert(categories);
+                //Hiển thị thông báo thành công
+                TempData["message"] = new XMessage("success", "Sửa loại sản phẩm thành công");
+
                 return RedirectToAction("Index");
             }
             return View(categories);
@@ -137,5 +171,78 @@ namespace THPTUDWeb.Areas.Admin.Controllers
             categoriesDAO.Delete(categories);
             return RedirectToAction("Index");
         }
+
+        //////////////////////////////////////////////////////////////////////
+        // GET: Admin/Category/Status/5
+        public ActionResult Status(int? id)
+        {
+            if (id == null)
+            {
+                //Thông báo thất bại
+                TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+                //Chuyển hướng trang
+                return RedirectToAction("Index", "Category");
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                //Thông báo thất bại
+                TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+                //Chuyển hướng trang
+                return RedirectToAction("Index", "Category");
+            }
+            //Cập nhật trạng thái
+            categories.Status = (categories.Status == 1) ? 2 : 1;
+            //Cập nhật UpdateBy
+            categories.UpdateBy = Convert.ToInt32(Session["UserId"].ToString());
+            //Cập nhật UpdateAt
+            categories.UpdateAt = DateTime.Now;
+            //Update Database
+            categoriesDAO.Update(categories);
+            //Hiển thị thông báo thành công
+            TempData["message"] = new XMessage("success", "Cập nhật trạng thái thành công");
+            //Trở về trang Index
+            return RedirectToAction("Index", "Category");
+        }
+
+        //////////////////////////////////////////////////////////////////////
+        // GET: Admin/Category/DelTrash/5
+        public ActionResult DelTrash(int? id)
+        {
+            if (id == null)
+            {
+                //Thông báo thất bại
+                TempData["message"] = new XMessage("danger", "Xoá loại sản phẩm thất bại");
+                //Chuyển hướng trang
+                return RedirectToAction("Index", "Category");
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                //Thông báo thất bại
+                TempData["message"] = new XMessage("danger", "Xoá loại sản phẩm thất bại");
+                //Chuyển hướng trang
+                return RedirectToAction("Index", "Category");
+            }
+            //Cập nhật trạng thái
+            categories.Status = 0;
+            //Cập nhật UpdateBy
+            categories.UpdateBy = Convert.ToInt32(Session["UserId"].ToString());
+            //Cập nhật UpdateAt
+            categories.UpdateAt = DateTime.Now;
+            //Update Database
+            categoriesDAO.Update(categories);
+            //Hiển thị thông báo thành công
+            TempData["message"] = new XMessage("success", "Xoá loại sản phẩm thành công");
+            //Trở về trang Index
+            return RedirectToAction("Index", "Category");
+        }
+        //////////////////////////////////////////////////////////////////////
+        // GET: Admin/Category/Trash : Thùng rác
+        public ActionResult Trash()
+        {
+            return View(categoriesDAO.getList("Trash"));
+        }
+
     }
 }
