5d3603bbc544594861f71eaa6d662917582748c2
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2023-10-28 19:03:11 +0700

    Init

--- THPTUDWeb/Properties/Settings.Designer.cs
+++ THPTUDWeb/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace THPTUDWeb.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-6HTEDCH\\MSSQLSERVER2019;Initial Catalog=master;Integrated Sec" +
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-9D2BFNE\\MSSQLSERVER2019;Initial Catalog=master;Integrated Sec" +
             "urity=True")]
         public string StrConnect {
             get {
